ab38291c70716afd3549593d23ff96cd27f02500
Gael Ceballos Nava <gaelceballosnava@gmail.com>
2023-03-06 14:42:40 -0600

    Lista de la compra con exepciones

--- lista_compra_avanzada.py
+++ lista_compra_avanzada.py
@@ -1,21 +1,39 @@
 SALIDA = "SALIR"
+Archivo_lista = "Lista de las compras.txt"
 
 def preguntar_producto_usuario():
     return input("Introduce un producto [{} para salir]".format(SALIDA))
 
 
+def Guardar_lista(lista_de_compra):
+    with open(Archivo_lista,"w") as mi_archivo:
+        mi_archivo.write("\n".join(lista_de_compra))
+    
+def guardar_item_en_lista(lista_de_compra,item_a_guardar):
+    if item_a_guardar.lower() in [a.lower() for a in lista_de_compra]:
+        print("Este producto ya esta agregado")
+    else:
+        lista_de_compra.append(item_a_guardar)
+
 def main():
     lista_de_compra = []
-    intput_usuario = preguntar_producto_usuario()
+    if input("Quieres cargar la ultima lista de la compra? [S/N]") == "S":
+        try:
+            with open(Archivo_lista, "r") as a:
+                lista_de_compra = a.read().split("\n")
+                print("\n".join(lista_de_compra))
+        except FileNotFoundError :
+            print("¡Archivo de la compra no encontrado!")
 
+    intput_usuario = preguntar_producto_usuario()
     while intput_usuario != SALIDA:
-        lista_de_compra.append(intput_usuario)
+        guardar_item_en_lista(lista_de_compra , intput_usuario)
         print("\n".join(lista_de_compra))
         intput_usuario = preguntar_producto_usuario()
+        
+    Guardar_lista(lista_de_compra)
     
-    a = open("Compra.txt","w")
-    a.write("\n".join(lista_de_compra))
-    a.close
+    
 
 if __name__ == '__main__':
     main()
